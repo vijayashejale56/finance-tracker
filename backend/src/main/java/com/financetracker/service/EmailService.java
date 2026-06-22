2369c97ba8d5a3ce24d0f4b4b90fa8c05fb6bf20
Nagesh Pujari <nagesh.p@aaespl.com>
2026-06-22 22:36:48 +0530

    autowired import issue resolved

--- backend/src/main/java/com/financetracker/service/EmailService.java
+++ backend/src/main/java/com/financetracker/service/EmailService.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
 
